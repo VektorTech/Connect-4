3777aa7a1f1132adf713a1d10940314a9e730490
VektorTech <kennymax18@gmail.com>
2022-11-30 16:16:12 -0500

    test scorePosition

--- src/__tests__/grid.test.ts
+++ src/__tests__/grid.test.ts
@@ -72,9 +72,9 @@ describe(".checkPlayerWinHorizontal()", () => {
       "%j, %j, %j",
     (...pos) => {
       const expected = pos.pop();
-      const currentIndex = pos as number[][];
+      const current = pos as number[][];
 
-      for (const index of currentIndex) {
+      for (const index of current) {
         grid.setIndexXY(index[0], index[1], "X");
       }
 
@@ -126,9 +126,9 @@ describe(".checkPlayerWinVertical()", () => {
       "%j, %j, %j",
     (...pos) => {
       const expected = pos.pop();
-      const currentIndex = pos as number[][];
+      const current = pos as number[][];
 
-      for (const index of currentIndex) {
+      for (const index of current) {
         grid.setIndexXY(index[0], index[1], "X");
       }
 
@@ -142,13 +142,13 @@ describe(".checkPlayerWinVertical()", () => {
 describe(".checkPlayerWinLeftDiag()", () => {
   const grid = new Grid(7, 5);
   const indices = [
-    [[0, 4], [1, 3], [2, 2], [3, 1], 4, false],
-    [[0, 4], [1, 2], [2, 2], [3, 1], 4, false],
-    [[0, 4], [2, 3], [2, 3], [3, 0], 4, false],
-    [[6, 4], [5, 3], [4, 2], [3, 1], 4, true],
-    [[6, 4], [5, 3], [4, 2], [3, 1], 3, true],
-    [[6, 4], [5, 3], [4, 2], [3, 1], 5, false],
-    [[4, 2], [3, 1], [2, 0], [0, 0], 3, true],
+    [[0, 4], [1, 3], [2, 2], [3, 1], 4, [], false],
+    [[0, 4], [1, 2], [2, 2], [3, 1], 4, [], false],
+    [[0, 4], [2, 3], [2, 3], [3, 0], 4, [], false],
+    [[6, 4], [5, 3], [4, 2], [3, 1], 4, [34, 26, 18, 10], true],
+    [[6, 4], [5, 3], [4, 2], [3, 1], 3, [34, 26, 18], true],
+    [[6, 4], [5, 3], [4, 2], [3, 1], 5, [], false],
+    [[4, 2], [3, 1], [2, 0], [0, 0], 3, [18, 10, 2], true],
   ];
 
   afterEach(() => {
@@ -159,15 +159,17 @@ describe(".checkPlayerWinLeftDiag()", () => {
     "checks whether player occupies n consecutive cells in left diagonal on a 7x5 grid: " +
       "%j, %j, %j, %j",
     (...pos) => {
-      const expected = pos.pop() as boolean;
+      const expectedBool = pos.pop() as boolean;
+      const expectedList = pos.pop() as number[];
       const inLine = pos.pop() as number;
-      const currentIndex = pos as number[][];
+      const current = pos as number[][];
 
-      for (const index of currentIndex) {
+      for (const index of current) {
         grid.setIndexXY(index[0], index[1], "X");
       }
 
-      expect(grid.checkPlayerWinLeftDiag("X", inLine)).toBe(expected);
+      expect(grid.checkPlayerWinLeftDiag("X", inLine)).toBe(expectedBool);
+      expect(grid.winningCells).toEqual(expectedList);
     }
   );
 });
@@ -175,9 +177,9 @@ describe(".checkPlayerWinLeftDiag()", () => {
 describe(".checkPlayerWinRightDiag()", () => {
   const grid = new Grid(7, 5);
   const indices = [
-    [[0, 4], [1, 3], [2, 2], [3, 1], true],
-    [[0, 4], [1, 2], [2, 2], [3, 1], false],
-    [[0, 4], [2, 3], [2, 3], [3, 0], false],
+    [[0, 4], [1, 3], [2, 2], [3, 1], 4, [28, 22, 16, 10], true],
+    [[0, 4], [1, 2], [2, 2], [3, 1], 4, [], false],
+    [[0, 4], [2, 3], [2, 3], [3, 0], 4, [], false],
   ];
 
   afterEach(() => {
@@ -188,14 +190,46 @@ describe(".checkPlayerWinRightDiag()", () => {
     "checks whether player occupies n consecutive cells in right diagonal on a 7x5 grid: " +
       "%j, %j, %j, %j",
     (...pos) => {
-      const expected = pos.pop() as boolean;
-      const currentIndex = pos as number[][];
+      const expectedBool = pos.pop() as boolean;
+      const expectedList = pos.pop() as number[];
+      const inLine = pos.pop() as number;
+      const current = pos as number[][];
 
-      for (const index of currentIndex) {
+      for (const index of current) {
         grid.setIndexXY(index[0], index[1], "X");
       }
 
-      expect(grid.checkPlayerWinRightDiag("X", 4)).toBe(expected);
+      expect(grid.checkPlayerWinRightDiag("X", inLine)).toBe(expectedBool);
+      expect(grid.winningCells).toEqual(expectedList);
+    }
+  );
+});
+
+describe(".scorePosition()", () => {
+  const grid = new Grid(8, 10);
+  const indices = [
+    [[4, 9], [4, 8], [3, 9], [5, 9], "X", 14],
+    [[0, 0], [3, 0], [2, 3], [5, 9], "O", 4],
+    [[2, 2], [3, 2], [3, 3], "X", 9],
+    [[2, 2], [3, 2], [3, 4], "X", 5],
+  ];
+
+  afterEach(() => {
+    return grid.reset();
+  });
+
+  it.each(indices)(
+    "evaluates the score for each player in the grid: " + "%j, %j, %j, %j",
+    (...pos) => {
+      const expected = pos.pop() as number;
+      const player = pos.pop() as string;
+      const current = pos as number[][];
+
+      for (const index of current) {
+        grid.setIndexXY(index[0], index[1], player);
+      }
+
+      expect(grid.scorePosition()[player]).toBe(expected);
     }
   );
 });
