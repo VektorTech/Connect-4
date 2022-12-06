7d603ae22fac7502d1fbfb324bc0366bd9e0cc8a
VektorTech <kennymax18@gmail.com>
2022-12-06 04:02:31 -0500

    controller to interface connect 4 logic with ui
    - add gameView ui class to construct grid and manage dom elements.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import "./style.css";
 
-import Game from "./game";
-
-import Grid from "./grid";
+import Connect4 from "./connect4";
+import GameView from "./ui/gameView";
+import dummyAI from "./dummyAI";
+import alphaBeta from "./alphaBeta";
 
 window.addEventListener("load", () => {
-  const grid = new Grid(3, 3);
-  console.table(grid.getLayout());
+  const message = document.getElementById("message");
+  const gameView = new GameView(Connect4.COLUMN_COUNT, Connect4.ROW_COUNT);
+  const connect4 = new Connect4();
+  gameView.generate(Connect4.COLUMN_COUNT, Connect4.ROW_COUNT);
+
+  gameView.addColumnClickHandler((columnNumber) => {
+    gameView.lock = true;
+    const row = connect4.commitPlay(columnNumber);
+    gameView.addDisc(columnNumber, row);
+    const win = connect4.checkForWin();
+
+    if (!win) {
+      setTimeout(async () => {
+        const columns = connect4.gridState.availableColumns;
+
+        if (columns.length) {
+          message.innerText = "AI Thinking...";
+          const col = (await alphaBeta(connect4.gridState)) as number;
+          message.innerText = "";
+          const row = connect4.commitPlay(col);
+          if (!connect4.checkForWin()) {
+            gameView.lock = false;
+          }
+          gameView.addDisc(col, row);
+        }
+      }, 400);
+    }
+  });
 });
---
+++ src/ui/gameView.ts
@@ -0,0 +1,84 @@
+import { getIndexAtPosition } from "../utils";
+
+export default class GameView {
+  private root: HTMLElement;
+  private grid: HTMLElement;
+  private discs: HTMLElement[];
+  private cells: HTMLElement[];
+  private colCount: number = 0;
+  private rowCount: number = 0;
+  private num: number = 0;
+  private columnClickHandlers: Function[] = [];
+  private discAdded = 0;
+
+  public lock = false;
+
+  constructor(col: number, row: number) {
+    this.colCount = col;
+    this.rowCount = row;
+    this.discs = new Array(col * row);
+    const _root = document.getElementById("game");
+    if (_root instanceof HTMLElement) {
+      this.root = _root;
+    } else {
+      throw new TypeError("Invalid Element Type");
+    }
+  }
+
+  generate(col: number, row: number) {
+    this.cells = [...new Array(col * row)].map((_) => {
+      const cell = document.createElement("div");
+      cell.className = "grid-cell";
+      return cell;
+    });
+    this.grid = document.createElement("div");
+    this.grid.className = "grid";
+    this.grid.style.setProperty("--col", col.toString());
+    this.grid.style.setProperty("--row", row.toString());
+    this.grid.append(...this.cells);
+    this.root.appendChild(this.grid);
+
+    this.root.addEventListener("click", (e) => {
+      if (!this.lock) {
+        const columnNumber = ~~(
+          ((e.clientX - this.root.offsetLeft) / this.root.offsetWidth) *
+          col
+        );
+        this.columnClickHandlers.forEach((handler) => handler(columnNumber));
+      }
+    });
+
+    addEventListener("game/win", (e: CustomEvent) => {
+      this.lock = true;
+      const { cells }: { cells: number[] } = e.detail;
+      setTimeout(() => {
+        cells.forEach((cell) => {
+          this.discs[cell]?.classList.add("disc-win");
+        });
+      }, 150);
+    });
+  }
+
+  addColumnClickHandler(handler: (column: number) => void) {
+    this.columnClickHandlers.push(handler);
+  }
+
+  addDisc(col: number, row: number) {
+    const disc = document.createElement("div");
+    disc.className = "disc" + (this.num ? " gold" : "");
+    disc.style.setProperty("--colOffset", col.toString());
+    disc.style.setProperty("--rowOffset", row.toString());
+    this.num = 1 - this.num;
+    this.grid.appendChild(disc);
+    const index = getIndexAtPosition(col, row, this.colCount);
+    this.discs[index] = disc;
+    disc.addEventListener("animationend", () => {
+      this.cells[index].appendChild(disc);
+    });
+    this.discAdded += 1;
+
+    if (this.discAdded == this.colCount * this.rowCount) {
+      this.lock = true;
+    }
+  }
+}
