4e68ece968fdb8f1fabcaf1afa72482ef2982c17
VektorTech <kennymax18@gmail.com>
2022-12-06 03:59:51 -0500

    add game class to manage current game state

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,62 @@
 import Grid from "./grid";
 
-export default class Game {}
+declare global {
+  var _grid: Grid;
+}
+
+export default class Game {
+  private grid: Grid;
+  private players: string[] = [];
+  private currentPlayer = 0;
+
+  constructor(gridCols: number, gridRows: number, matchInLine: number) {
+    this.grid = new Grid(gridCols, gridRows, matchInLine);
+    window._grid = this.grid;
+  }
+
+  addPlayer(playerID: string) {
+    this.players.push(playerID);
+  }
+
+  getWinnerInfo() {
+    return {
+      winner: this.players[this.currentPlayer],
+      cells: this.grid.winningCells,
+    };
+  }
+
+  get gridState() {
+    return this.grid;
+  }
+
+  commitPlay(column: number) {
+    const rowIndex = this.grid.dropDisc(column, this.currentPlayer + 1);
+    this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+    return rowIndex;
+  }
+
+  checkForWin() {
+    const win =
+      this.grid.checkPlayerWinHorizontal(
+        this.players.length - this.currentPlayer
+      ) ||
+      this.grid.checkPlayerWinVertical(
+        this.players.length - this.currentPlayer
+      ) ||
+      this.grid.checkPlayerWinLeftDiag(
+        this.players.length - this.currentPlayer
+      ) ||
+      this.grid.checkPlayerWinRightDiag(
+        this.players.length - this.currentPlayer
+      );
+
+    if (win) {
+      const event = new CustomEvent("game/win", {
+        detail: this.getWinnerInfo(),
+      });
+      dispatchEvent(event);
+    }
+
+    return win;
+  }
+}
