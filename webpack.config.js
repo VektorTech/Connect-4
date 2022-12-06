af7b728482a5b2d70ba5f0763d9a3dd55ce324cc
VektorTech <kennymax18@gmail.com>
2022-12-06 03:49:19 -0500

    add grid class & ai worker as separate scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	mode: 'development',
-	output: {
-		path: path.resolve(__dirname, "public"),
-		filename: 'script.bundle.js',
-		publicPath: '/',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './public',
-		hot: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.ts?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.ts', '.js'],
-	}
+  mode: "development",
+  entry: {
+    script: "./src/index.ts",
+    grid: "./src/grid.ts",
+    alphaBetaWorker: "./src/alphaBetaWorker.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].bundle.js",
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./public",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
