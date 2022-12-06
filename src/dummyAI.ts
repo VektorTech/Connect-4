72144a2f9da97ec7ae1c3b7f438886454cd7a9fb
VektorTech <kennymax18@gmail.com>
2022-12-06 07:53:50 -0500

    accept dummy ai threshold as input to func

--- src/dummyAI.ts
+++ src/dummyAI.ts
@@ -1,11 +1,11 @@
 import Grid from "./grid";
 
-export default function dummyAI(grid: Grid) {
+export default async function dummyAI(grid: Grid, threshold: number) {
   const randPos = ~~(Math.random() * grid.availableColumns.length);
   let bestMove = grid.availableColumns[randPos];
   let bestScore = 0;
 
-  if (Math.random() < 0.7) {
+  if (Math.random() < threshold) {
     for (const col of grid.availableColumns) {
       const cloned = grid.clone();
       cloned.dropDisc(col, 2);
