162f76864a50e1f3f204b26b5130f4807ca1918f
VektorTech <kennymax18@gmail.com>
2022-12-06 07:52:18 -0500

    make ai function cancellable with abort controller

--- src/alphaBeta.ts
+++ src/alphaBeta.ts
@@ -5,79 +5,21 @@ const AI = 2;
 
 const AIWorker = new Worker("./alphaBetaWorker.bundle.js");
 
-export default function alphaBeta(grid: Grid) {
-  return new Promise((resolve, reject) => {
+export default function alphaBeta(grid: Grid, signal: AbortSignal, depth: number) {
+  const error = new DOMException("Aborted Calculation in Worker", "AbortError");
+  return new Promise<number>((resolve, reject) => {
+    if (signal.aborted) {
+      return reject(error);
+    }
+
     if (window.Worker) {
-      AIWorker.postMessage(grid);
+      AIWorker.postMessage([grid, depth]);
       AIWorker.onmessage = (e) => {
-        resolve(e.data);
+        resolve(e.data as number);
       };
+      signal.addEventListener("abort", () => {
+        reject(error);
+      });
     }
   });
-}
-
-function alphaBetaAI(
-  grid: Grid,
-  depth = 6,
-  alpha = -Infinity,
-  beta = Infinity,
-  isMaximizingPlayer = true
-) {
-  const currentPlayer = isMaximizingPlayer ? AI : HUMAN;
-  const lastPlayer = isMaximizingPlayer ? HUMAN : AI;
-
-  let bestMove: number;
-  let bestScore = isMaximizingPlayer ? -Infinity : Infinity;
-
-  if (depth == 0 || isWinner(grid, lastPlayer) || grid.isGridFilled()) {
-    const score = evaluatePosition(grid) * (depth + 1);
-    return [undefined, score];
-  }
-
-  for (const col of grid.availableColumns) {
-    const cloned = grid.clone();
-    cloned.dropDisc(col, currentPlayer);
-    const [_, _score] = alphaBetaAI(
-      cloned,
-      depth - 1,
-      alpha,
-      beta,
-      !isMaximizingPlayer
-    );
-
-    if (isMaximizingPlayer) {
-      if (_score > bestScore) {
-        bestScore = _score;
-        bestMove = col;
-      }
-      alpha = Math.max(alpha, bestScore);
-    } else {
-      if (_score < bestScore) {
-        bestScore = _score;
-        bestMove = col;
-      }
-      beta = Math.min(beta, bestScore);
-    }
-
-    if (beta <= alpha) break;
-  }
-
-  return [bestMove, bestScore];
-}
-
-function isWinner(grid: Grid, player: number) {
-  return (
-    grid.checkPlayerWinHorizontal(player) ||
-    grid.checkPlayerWinVertical(player) ||
-    grid.checkPlayerWinLeftDiag(player) ||
-    grid.checkPlayerWinRightDiag(player)
-  );
-}
-
-function evaluatePosition(grid: Grid) {
-  const score = grid.scorePosition;
-  const AIScore = score[AI] + Number(isWinner(grid, AI)) * 10000;
-  const HUMANScore = score[HUMAN] + Number(isWinner(grid, HUMAN)) * 10000;
-
-  return AIScore - HUMANScore;
 }
--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,75 @@ import Connect4 from "./connect4";
 import GameView from "./ui/gameView";
 import dummyAI from "./dummyAI";
 import alphaBeta from "./alphaBeta";
+import Grid from "./grid";
+
+let timeout: NodeJS.Timeout;
+let abortController: AbortController;
+let currentAI = 4;
+
+const AIList = [
+  (grid: Grid, signal: AbortSignal) => dummyAI(grid, 0),
+  (grid: Grid, signal: AbortSignal) => dummyAI(grid, 0.7),
+  (grid: Grid, signal: AbortSignal) => alphaBeta(grid, signal, 2),
+  (grid: Grid, signal: AbortSignal) => alphaBeta(grid, signal, 3),
+  (grid: Grid, signal: AbortSignal) => alphaBeta(grid, signal, 5),
+  (grid: Grid, signal: AbortSignal) => alphaBeta(grid, signal, 6),
+  (grid: Grid, signal: AbortSignal) => alphaBeta(grid, signal, 8),
+];
 
 window.addEventListener("load", () => {
+  const resetButton = document.getElementById("reset");
+  const selectDifficulty = document.getElementById("difficulty");
   const message = document.getElementById("message");
   const gameView = new GameView(Connect4.COLUMN_COUNT, Connect4.ROW_COUNT);
   const connect4 = new Connect4();
   gameView.generate(Connect4.COLUMN_COUNT, Connect4.ROW_COUNT);
 
   gameView.addColumnClickHandler((columnNumber) => {
+    abortController = new AbortController();
     gameView.lock = true;
     const row = connect4.commitPlay(columnNumber);
     gameView.addDisc(columnNumber, row);
     const win = connect4.checkForWin();
 
     if (!win) {
-      setTimeout(async () => {
-        const columns = connect4.gridState.availableColumns;
+      timeout = setTimeout(async () => {
+        try {
+          const columns = connect4.gridState.availableColumns;
 
-        if (columns.length) {
-          message.innerText = "AI Thinking...";
-          const col = (await alphaBeta(connect4.gridState)) as number;
-          message.innerText = "";
-          const row = connect4.commitPlay(col);
-          if (!connect4.checkForWin()) {
-            gameView.lock = false;
+          if (columns.length) {
+            message.innerText = "AI is thinking, please wait...";
+            const col = await AIList[currentAI](
+              connect4.gridState,
+              abortController.signal
+            );
+            message.innerText = "";
+            const row = connect4.commitPlay(col);
+            if (!connect4.checkForWin()) {
+              gameView.lock = false;
+            }
+            gameView.addDisc(col, row);
           }
-          gameView.addDisc(col, row);
+        } catch (e) {
+          console.log(e);
+        } finally {
+          abortController = null;
         }
       }, 400);
     }
+  });
+
+  resetButton.addEventListener("click", () => {
+    message.innerHTML = "";
+    connect4.reset();
+    gameView.clear();
+    clearTimeout(timeout);
+    if (abortController) {
+      abortController.abort();
+    }
+  });
+
+  selectDifficulty.addEventListener("change", (e: InputEvent) => {
+    currentAI = +(e.target as HTMLSelectElement).value;
   });
 });
