38a2c0a18cb508026a5bc990e0fdac6dae49de89
VektorTech <kennymax18@gmail.com>
2022-12-06 04:03:08 -0500

    add connect4 class

---
+++ src/connect4.ts
@@ -0,0 +1,13 @@
+import Game from "./game";
+
+export default class Connect4 extends Game {
+  static COLUMN_COUNT = 7;
+  static ROW_COUNT = 6;
+  static CONNECT = 4;
+
+  constructor() {
+    super(Connect4.COLUMN_COUNT, Connect4.ROW_COUNT, Connect4.CONNECT);
+    this.addPlayer("John");
+    this.addPlayer("Fred");
+  }
+}
